204accd0e2b262e055bfea8243f09b1ed582eef6
liaokongVFX <568250549@qq.com>
2021-09-03 22:56:27 +0800

    添加了一个测试例子

--- test5.py
+++ test5.py
@@ -9,3 +9,8 @@ from ftrack_events_helper import subscribe, logger
 @subscribe(priority=50)
 def test5(session, event):
     logger.exception('这是test5的log')
+
+
+@subscribe()
+def test6(session, event):
+    logger.info('这是test6的log')
