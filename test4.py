f36073278482e0fcd9c844b6cb59d7dd081eacff
liaokongVFX <568250549@qq.com>
2021-09-05 19:58:19 +0800

    添加了一个测试例子

--- test4.py
+++ test4.py
@@ -10,3 +10,11 @@ from ftrack_events_helper import EventBase, subscribe
 class Test4(EventBase):
     def run(self, event):
         self.logger.warning('这是test4的log')
+
+
+@subscribe()
+class Test7(EventBase):
+    event_name = '牛逼777'
+
+    def run(self, event):
+        self.logger.warning('这是牛逼777的log')
